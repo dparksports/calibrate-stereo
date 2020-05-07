ce54393f8a5779be5b0bca13dc125bbe2353e8b9
Dan Park <dpark.sports@gmail.com>
2020-05-06 18:14:23 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/3dreconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3dreconstruction"
+  "3dreconstruction.pdb"
+  "CMakeFiles/3dreconstruction.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3dreconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
